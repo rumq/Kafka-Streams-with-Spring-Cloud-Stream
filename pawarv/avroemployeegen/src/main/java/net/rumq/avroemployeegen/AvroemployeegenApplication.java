0e683536d939588c5ba223521a39ac8a204ff112
PawarV <pawar@rumq.net>
2023-04-01 15:48:43 +0100

    Issue #3 - Create and send employee

--- pawarv/avroemployeegen/src/main/java/net/rumq/avroemployeegen/AvroemployeegenApplication.java
+++ pawarv/avroemployeegen/src/main/java/net/rumq/avroemployeegen/AvroemployeegenApplication.java
@@ -1,13 +1,42 @@
 package net.rumq.avroemployeegen;
 
+import net.rumq.avroemployeegen.services.KafkaProducerService;
+import net.rumq.demo.kafka.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AvroemployeegenApplication {
+public class AvroemployeegenApplication implements ApplicationRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AvroemployeegenApplication.class, args);
-	}
+    @Autowired
+    private KafkaProducerService producerService;
 
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvroemployeegenApplication.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        String run = "a-";
+        producerService.sendMessage(createEmployee(run + "1111", "Anand", "Audio", 1111));
+        producerService.sendMessage(createEmployee(run + "2222", "Bacchan", "Brand", 2222));
+        producerService.sendMessage(createEmployee(run + "3333", "Chand", "Channel", 3333));
+
+
+    }
+
+    private Employee createEmployee(String id, String name, String department, int salary) {
+        Employee employee = new Employee();
+        employee.setId(id);
+        employee.setName(name);
+        employee.setDepartment(department);
+        employee.setSalary(salary);
+
+        return employee;
+
+    }
 }
---
+++ pawarv/avroemployeegen/src/main/java/net/rumq/avroemployeegen/services/KafkaProducerService.java
@@ -0,0 +1,25 @@
+package net.rumq.avroemployeegen.services;
+
+import lombok.extern.log4j.Log4j2;
+import net.rumq.demo.kafka.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Log4j2
+public class KafkaProducerService {
+    @Value("${application.configs.topic.name}")
+    private String TOPIC_NAME;
+
+    @Autowired
+    private KafkaTemplate<String, Employee> kafkaTemplate;
+
+    public void sendMessage(Employee employee) {
+        log.info(String.format("Producing Employee No : %s", employee.getId()));
+        kafkaTemplate.send(TOPIC_NAME, employee.getId(), employee);
+
+
+    }
+}
