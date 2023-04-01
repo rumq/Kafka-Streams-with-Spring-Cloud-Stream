3ef878cb3730217813086b60a1a3a66adeb05813
PawarV <pawar@rumq.net>
2023-04-01 14:32:49 +0100

    Issue #3 Start avro employee gen

---
+++ pawarv/avroemployeegen/src/main/java/net/rumq/avroemployeegen/AvroemployeegenApplication.java
@@ -0,0 +1,13 @@
+package net.rumq.avroemployeegen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvroemployeegenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvroemployeegenApplication.class, args);
+	}
+
+}
---
+++ pawarv/avroemployeegen/src/test/java/net/rumq/avroemployeegen/AvroemployeegenApplicationTests.java
@@ -0,0 +1,13 @@
+package net.rumq.avroemployeegen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvroemployeegenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
